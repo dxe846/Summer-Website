f413ffd29897b68667da580d65b7ad72cfcb8eea
dxe846 <dxe846@student.bham.ac.uk>
2020-06-27 16:49:49 +0100

    Modified templates to display posts from database.

--- Summer_Project_2020/settings.py
+++ Summer_Project_2020/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.blogConfig',
+    'blog.apps.BlogConfig',
 ]
 
 # Middleware framework
---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\DXE846\WebProject Summer 2020\Summer Project 2020\Summer Project 2020\myvenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
